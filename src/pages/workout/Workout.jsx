c96920f05c97e0fb833b3994a17ec742a8dfde66
Paul Palkin <111692145+Bestpaul73@users.noreply.github.com>
2024-02-18 21:18:55 +0300

    add Store

--- src/pages/workout/Workout.jsx
+++ src/pages/workout/Workout.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { courses } from "./MocData";
-import style from "./Workout.module.scss";
-import { Logo } from "../../UI/Logo/Logo";
-import { Button } from "../../UI/Button/Button";
-import phone from "./phone.png";
-import { WorkoutCardImg } from "./WorkoutCardImg/WorkoutCardImg";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { courses } from './MocData';
+import style from './Workout.module.scss';
+import { Logo } from '../../UI/Logo/Logo';
+import { Button } from '../../UI/Button/Button';
+import phone from './phone.png';
+import { WorkoutCardImg } from './WorkoutCardImg/WorkoutCardImg';
+import { useSelector } from 'react-redux';
 
 export const Workout = () => {
+  const userName = useSelector((state) => state.userApp.userName);
+  console.log(userName);
+
   const params = useParams();
   const course = courses.find((course) => course.nameEN === params.id);
   return (
@@ -18,7 +22,7 @@ export const Workout = () => {
       <main>
         <section className={style.workoutCard}>
           <h1 className={style.workoutCard_title}>{course.nameRU}</h1>
-          <WorkoutCardImg worcout={course.nameEN}/>
+          <WorkoutCardImg worcout={course.nameEN} />
           {/* <img src={Yoga} alt="" className={style.workoutCard_img}></img> */}
         </section>
         <section className={style.fitting}>
@@ -28,10 +32,7 @@ export const Workout = () => {
               return (
                 <div className={style.criterion}>
                   <div className={style.criterion_counter}>
-                    <p className={style.criterion_counterText}>
-                      {" "}
-                      {course.fitting.indexOf(el) + 1}
-                    </p>
+                    <p className={style.criterion_counterText}> {course.fitting.indexOf(el) + 1}</p>
                   </div>
                   <div className={style.criterion_text}>
                     <p className={style.basicText}>{el}</p>
@@ -45,25 +46,18 @@ export const Workout = () => {
           <h2 className={style.section_title}>Направления:</h2>
           <div className={style.directions_textBox}>
             {course.directions.map((el) => {
-              return (
-                <p className={style.basicText}>
-                  &nbsp; &nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;{el}
-                </p>
-              );
+              return <p className={style.basicText}>&nbsp; &nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;{el}</p>;
             })}
           </div>
         </section>
-        <p className={style.workoutDescription}>
-          &nbsp;&nbsp;&nbsp;{course.description}
-        </p>
+        <p className={style.workoutDescription}>&nbsp;&nbsp;&nbsp;{course.description}</p>
         <section className={style.feedback}>
           <p className={style.feedback_text}>
-            Оставьте заявку на пробное занятие, мы свяжемся <br /> с вами,
-            поможем с выбором направления и тренера, с которым тренировки
-            принесут здоровье и радость!
+            Оставьте заявку на пробное занятие, мы свяжемся <br /> с вами, поможем с выбором направления и тренера, с
+            которым тренировки принесут здоровье и радость!
           </p>
-          <Button children={"Записаться на тренировку"} />
-          <img src={phone} alt="" className={style.feedback_img} />
+          <Button children={'Записаться на тренировку'} />
+          <img src={phone} alt='' className={style.feedback_img} />
         </section>
       </main>
     </div>
