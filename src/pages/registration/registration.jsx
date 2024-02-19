6de9fc28ad9b2620dbd6219bc223b531c6a0f01e
EkaFord <138514574+EkaFord@users.noreply.github.com>
2024-02-19 12:16:00 -0500

    new pages and updates routes

---
+++ src/pages/registration/registration.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import style from "./Registration.module.scss";
+import { Logo } from "../../UI/Logo/Logo";
+import { Button } from "../../UI/Button/Button";
+
+const LoginSignup = () => {
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default LoginSignup
+
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,14 @@
 import {Routes, Route } from "react-router-dom";
 import { Workout } from "./pages/workout/Workout";
 import { Main } from "./pages/main/main";
+import { LoginSignup } from "./pages/registration";
 
 export const AppRoutes = () => {
   return (
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/workout/:id" element={<Workout />} />
+        <Route path="/" element={<LoginSignup />} />
       </Routes>
   );
 };
