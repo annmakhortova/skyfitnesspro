8844a906606969c815f252053ab57836373710a4
EkaFord <138514574+EkaFord@users.noreply.github.com>
2024-02-19 20:55:22 -0500

    layout

--- src/pages/registration/registration.jsx
+++ src/pages/registration/registration.jsx
@@ -1,17 +1,38 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import style from "./Registration.module.scss";
-import { Logo } from "../../UI/Logo/Logo";
 import { Button } from "../../UI/Button/Button";
+import { Logo } from "../../UI/Logo/Logo";
+import style from "./Registration.module.scss";
 
 const LoginSignup = () => {
+
     return (
-        <div>
-
-
+        <div className={style.container}>
+            <header>
+              <Logo className={style.logo} />
+            </header>
+    
+            <div className={style.inputs}>
+                <div className={style.input}>
+                    <input type="Логин" placeholder="Логин"/>
+                </div>
+                <div className={style.input}>
+                    <input type="Пароль"placeholder="Пароль"/>
+                </div>
+                <div className={style.input}>
+                    <input type="Повторите пароль" placeholder="Повторите пароль"/>
+                </div>
+            </div>
+    
+            <div className={style.buttonsContainer}>
+             <Button children={"Войти"} />
+             <button className={style.registerButton}>Зарегистрироваться</button>
+            </div>
+    
         </div>
     )
+    
 }
+
 
 export default LoginSignup
 
