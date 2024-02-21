29bd3aed6750704cde18b739d9e2345ef53ccba9
Iss1937 <133023027+Iss1937@users.noreply.github.com>
2024-02-22 01:21:15 +0300

    add training page

---
+++ src/pages/training/MocData.js
@@ -0,0 +1,6 @@
+export const courses = {
+  id: 123,
+  progress_1: 10,
+  progress_2: 10,
+  progress_3: 10,
+};
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,14 @@
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Workout } from "./pages/workout/Workout";
+import { Training } from "./pages/training/Training";
 import { Main } from "./pages/main/main";
 
 export const AppRoutes = () => {
   return (
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/workout/:id" element={<Workout />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/workout/:id" element={<Workout />} />
+      <Route path="/training" element={<Training />} />
+    </Routes>
   );
 };
