efbface07864d9f5598fcace0313f960b4777461
Paul Palkin <111692145+Bestpaul73@users.noreply.github.com>
2024-02-24 18:04:43 +0300

    fb

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore} from 'firebase/firestore/lite';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnUfKDgZf8hanKMA1DIrzhVZyE7HOMD5A",
+  authDomain: "fitness-pro-9efbb.firebaseapp.com",
+  databaseURL: "https://fitness-pro-9efbb-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fitness-pro-9efbb",
+  storageBucket: "fitness-pro-9efbb.appspot.com",
+  messagingSenderId: "986522955912",
+  appId: "1:986522955912:web:692750562c74adb321af3d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
+
+// export const auth = getAuth(app)
