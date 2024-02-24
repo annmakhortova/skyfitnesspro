95001b39a0ac83d11b9b914a96415c354ad67f0b
Paul Palkin <111692145+Bestpaul73@users.noreply.github.com>
2024-02-24 15:08:03 +0300

    1507

--- src/pages/workout/Workout.jsx
+++ src/pages/workout/Workout.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useSelector } from 'react';
 import { useParams } from 'react-router-dom';
 
 import style from './Workout.module.scss';
