61b63902548eebed1d5666d1ea1fb4d51bf99caa
MarinaSkorik96 <138443386+MarinaSkorik96@users.noreply.github.com>
2024-02-26 23:34:14 +0300

    Попытки собрать массив

--- src/pages/selectWorkout/SelectWorkout.jsx
+++ src/pages/selectWorkout/SelectWorkout.jsx
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { workouts } from "./WorkoutsMocData";
 import { courses } from "../workout/MocData";
 import style from "./SelectWorkout.module.scss";
+import { getAllCourses, getAllWorkouts } from "../api";
 // import made from "./made.png";
 // import { getAllCourses } from "../api";
 
-
 export const SelectWorkout = () => {
+  const params = "Yoga";
+  const [course, setCourse] = useState();
+  const [currentWorkoutsArr, setCurrentWorkoutsArr] = useState([]);
+
+  useEffect(() => {
+    // console.log(params);
+    getAllCourses()
+      .then((courses) => {
+        console.log("Курсы:", courses);
+        setCourse(
+          Object.values(courses).find((course) => course.nameEN === params)
+        );
+      })
+      .catch(() => {})
+      .finally(() => {
+        // console.log(course)
+      });
+  }, [params.id]);
+
+  // useEffect(()=>{
+  getAllWorkouts().then((workoutss) => {
+    console.log();
+  });
+  // })
+
+  useEffect(() => {
+    // console.log(1);
+    // console.log(course);
+    if (course) {
+      // console.log(2);
+
+      course.workouts.forEach((id) => {
+        // setCurrentWorkoutsArr([])
+
+        // console.log(id);
+        workouts.map((work) => {
+          // console.log(work);
+          if (id === work._id) {
+            // console.log(work);
+            // console.log(currentWorkoutsArr)
+            // currentWorkoutsArr.push(work)
+
+            ;
+            // console.log(work);
+            return setCurrentWorkoutsArr([...currentWorkoutsArr, work]);
+
+          }
+
+        });
+
+      });
+    }
+  }, [course]);
+
+  useEffect(() => {
+    console.log(currentWorkoutsArr);
+  }, [currentWorkoutsArr]);
 
   const currentCourse = "Йога";
   const currentWorkoutsId = courses.find(
