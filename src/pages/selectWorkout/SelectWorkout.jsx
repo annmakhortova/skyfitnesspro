9815c0ba1451589200b6c7e5571ca04d752d2b75
MarinaSkorik96 <138443386+MarinaSkorik96@users.noreply.github.com>
2024-02-27 20:49:26 +0300

    Верная фильтрация воркаутов

--- src/pages/selectWorkout/SelectWorkout.jsx
+++ src/pages/selectWorkout/SelectWorkout.jsx
@@ -1,18 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { workouts } from "./WorkoutsMocData";
-import { courses } from "../workout/MocData";
 import style from "./SelectWorkout.module.scss";
 import { getAllCourses, getAllWorkouts } from "../api";
-// import made from "./made.png";
-// import { getAllCourses } from "../api";
 
 export const SelectWorkout = () => {
-  const params = "Yoga";
+  const params = "StepAirobic";
   const [course, setCourse] = useState();
+  const [allWorkouts, setAllWotkouts] = useState();
   const [currentWorkoutsArr, setCurrentWorkoutsArr] = useState([]);
 
   useEffect(() => {
-    // console.log(params);
     getAllCourses()
       .then((courses) => {
         console.log("Курсы:", courses);
@@ -22,62 +18,29 @@ export const SelectWorkout = () => {
       })
       .catch(() => {})
       .finally(() => {
-        // console.log(course)
       });
   }, [params.id]);
 
-  // useEffect(()=>{
-  getAllWorkouts().then((workoutss) => {
-    console.log();
-  });
-  // })
+  useEffect(() => {
+    getAllWorkouts().then((workoutss) => {
+      setAllWotkouts(Object.values(workoutss));
+    });
+  }, []);
 
   useEffect(() => {
-    // console.log(1);
-    // console.log(course);
     if (course) {
-      // console.log(2);
-
-      course.workouts.forEach((id) => {
-        // setCurrentWorkoutsArr([])
-
-        // console.log(id);
-        workouts.map((work) => {
-          // console.log(work);
-          if (id === work._id) {
-            // console.log(work);
-            // console.log(currentWorkoutsArr)
-            // currentWorkoutsArr.push(work)
-
-            ;
-            // console.log(work);
-            return setCurrentWorkoutsArr([...currentWorkoutsArr, work]);
-
-          }
-
-        });
-
-      });
+      setCurrentWorkoutsArr(allWorkouts.filter((e) =>
+      course.workouts.includes(e._id))
+      );
     }
-  }, [course]);
+  }, [allWorkouts, course]);
 
-  useEffect(() => {
-    console.log(currentWorkoutsArr);
-  }, [currentWorkoutsArr]);
-
-  const currentCourse = "Йога";
-  const currentWorkoutsId = courses.find(
-    (course) => course.nameRU === currentCourse
-  ).workouts;
-  const currentWorkouts = workouts.filter((e) =>
-    currentWorkoutsId.includes(e._id)
-  );
   return (
     <div className={style.selectWorkout}>
       <div className={style.selectWorkout_box}>
         <h1 className={style.selectWorkout_title}>Выберите тренировку</h1>
         <div className={style.workouts}>
-          {currentWorkouts.map((el) => {
+          {currentWorkoutsArr.map((el) => {
             return (
               <div className={style.workout}>
                 <p className={style.workout_text}>{el.name}</p>
