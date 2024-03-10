e21bf170be97674f0003ba077e2a907f9bd9288a
Paul Palkin <111692145+Bestpaul73@users.noreply.github.com>
2024-03-10 11:58:16 +0300

    WorkoutPurchasedPopup

--- src/pages/workout/WorkoutPurchased.jsx
+++ src/pages/workout/WorkoutPurchased.jsx
@@ -1,32 +1,34 @@
 import React, { useEffect } from 'react';
 import styles from '../progress/Progress.module.scss';
 import Hand from '../progress/Hand.png';
-import { hidePopupFlag } from '../../components/hidePopup/hidePopupFlag';
+// import { hidePopupFlag } from '../../components/hidePopup/hidePopupFlag';
 import { useNavigate } from 'react-router-dom';
 
 export const WorkoutPurchased = () => {
   const navigate = useNavigate();
-  const hidePopup = (e, type) => {
-    if (hidePopupFlag(e, type)) navigate(-1);
+  
+  const hidePopup = () => {
+     navigate(-1);
   };
 
-  const onFocusFirstInput = () => {
-    const firstInputEl = document.getElementsByTagName('input');
-    firstInputEl[0]?.focus();
-  };
-  useEffect(() => onFocusFirstInput(), []);
+  // const onFocusFirstInput = () => {
+  //   const firstInputEl = document.getElementsByTagName('imp');
+  //   firstInputEl[0]?.focus();
+  // };
+  // useEffect(() => onFocusFirstInput(), []);
+
   return (
     <div
       className={styles.popup_wrapper}
-      onMouseUp={(e) => hidePopup(e, 'mouse')}
-      onKeyDown={(e) => hidePopup(e, 'kbd')}
+      onClick={() => hidePopup()}
+      onKeyDown={() => hidePopup()}
     >
       <div className={styles.progressForm} id='#popup'>
         <div className={styles.page}>
           <div className={styles.progressCheckForm}>
             <div className={styles.headerProgress}>Вы купили курс!</div>
             <div className={styles.imgBox}>
-              <img className={styles.Hand} src={Hand} alt='Hand' />
+              <img autoFocus className={styles.Hand} src={Hand} alt='Hand' />
             </div>
           </div>
         </div>
