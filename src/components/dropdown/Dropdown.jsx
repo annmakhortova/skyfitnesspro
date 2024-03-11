766f76694a944471a6f77fccb80949402790dd57
MarinaSkorik96 <138443386+MarinaSkorik96@users.noreply.github.com>
2024-03-11 13:14:50 +0300

    Marina/ fix purchase rate

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -14,6 +14,7 @@ export function Dropdown() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  
   function handleSignOut() {
     const auth = getAuth();
     signOut(auth)
