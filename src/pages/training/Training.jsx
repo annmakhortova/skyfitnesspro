69683ac651fd68aaa84252b4a915c4716e272151
Paul Palkin <111692145+Bestpaul73@users.noreply.github.com>
2024-03-11 14:35:29 +0300

    player

--- src/pages/training/Training.jsx
+++ src/pages/training/Training.jsx
@@ -68,7 +68,7 @@ export const Training = () => {
       <main>
         <h1 className={style.nameTraining}>{courseName}</h1>
         <h2 className={style.dateLink}>{workoutName}</h2>
-        {/* <ReactPlayer url={workoutVideo} width='100%' height='720px' /> */}
+        <ReactPlayer url={workoutVideo} width='100%' height='720px' />
 
         <section className={style.resultSection}>
           {workoutExercises ? (
